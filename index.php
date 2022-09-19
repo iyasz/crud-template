12c8dea1ba5039b4fc44b33e6fb41bff5303daca
Yasz Avellia <zakamaragames@gmail.com>
2022-09-19 18:54:20 +0700

    menambah form

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD TEMPLATE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+    <style>
+        .container {
+            top: 90px;
+        }
+    </style>
+    <div class="container position-relative ">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="mb-3">
+                                <label for="">Nama Lengkap</label>
+                                <input type="text" placeholder="" name="" id="" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="">NIS</label>
+                                <input type="text" placeholder="" name="" id="" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="">No Telp</label>
+                                <input type="text" placeholder="" name="" id="" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Asal Sekolah</label>
+                                <input type="text" placeholder="" name="" id="" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Alamat</label>
+                                <input type="text" placeholder="" name="" id="" class="form-control">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+
+</html>
